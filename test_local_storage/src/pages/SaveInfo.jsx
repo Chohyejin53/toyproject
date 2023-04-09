9e2ffb25ca35bd3f70feb3278a2eac8941a6031c
rameeeee <moderato611@naver.com>
2023-04-09 18:31:10 +0900

    [230409] 삭제 버튼 추가

--- test_local_storage/src/pages/SaveInfo.jsx
+++ test_local_storage/src/pages/SaveInfo.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 
 const SaveInfo = () => {
   // 로컬 스토리지에서 저장된 모든 값을 가져와 배열로 저장
-  const savedInputs = Object.entries(localStorage)
-    .filter(([key, value]) => key.startsWith("saveInput"))
-    .map(([key, value]) => value);
+  const [savedInputs, setSavedInputs] = useState(
+    Object.entries(localStorage)
+      .filter(([key, value]) => key.startsWith("saveInput"))
+      .map(([key, value]) => ({ key, value }))
+  );
 
   // Base64 문자열을 이미지 파일 URL로 변환하는 함수
   const base64ToImage = (base64String) => {
@@ -17,18 +19,34 @@ const SaveInfo = () => {
     return URL.createObjectURL(blob);
   };
 
+  // 삭제 버튼 클릭 이벤트 핸들러
+  const handleRemove = (key) => {
+    localStorage.removeItem(key);
+    setSavedInputs((prevState) =>
+      prevState.filter((savedInput) => savedInput.key !== key)
+    );
+  };
+
+  // 전체 삭제 버튼 클릭 이벤트 핸들러
+  const handleAllRemove = () => {
+    localStorage.clear();
+    setSavedInputs([]);
+  };
+
   return (
     <div>
       <h1>저장된 값 목록</h1>
+      <button onClick={handleAllRemove}>전체 삭제</button>
       <ul>
         {/* 로컬 스토리지에서 가져온 값들을 순회하며 리스트 아이템으로 출력 */}
-        {savedInputs.map((savedInput, index) => (
+        {savedInputs.map(({ key, value }, index) => (
           <li key={index}>
-            {savedInput.startsWith("data:image") ? (
-              <img src={base64ToImage(savedInput)} alt="저장된 이미지" />
+            {value.startsWith("data:image") ? (
+              <img src={base64ToImage(value)} alt="저장된 이미지" />
             ) : (
-              savedInput
+              value
             )}
+            <button onClick={() => handleRemove(key)}>삭제</button>
           </li>
         ))}
       </ul>
