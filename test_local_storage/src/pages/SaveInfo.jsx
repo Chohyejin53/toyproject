ea4a89cf3dad6ea7f1c81588122a713f86b723c1
rameeeee <moderato611@naver.com>
2023-04-11 01:05:26 +0900

    [230410] local storage 테스트

--- test_local_storage/src/pages/SaveInfo.jsx
+++ test_local_storage/src/pages/SaveInfo.jsx
@@ -1,214 +1,74 @@
-// import React, { useState, useEffect } from "react";
-// import "./SaveInfo.scss";
-
-// const SaveInfo = () => {
-//   const [dataList, setDataList] = useState([]);
-
-//   useEffect(() => {
-//     const keys = Object.keys(localStorage).filter(
-//       (key) => key.startsWith("bar_") || key.startsWith("about_")
-//     );
-//     const dataList = keys.map((key) => {
-//       const data = JSON.parse(localStorage.getItem(key));
-//       return { ...data, key };
-//     });
-//     setDataList(dataList);
-//   }, []);
-
-//   const handleDeleteClick = (key) => {
-//     localStorage.removeItem(key);
-//     setDataList((prevDataList) =>
-//       prevDataList.filter((data) => data.key !== key)
-//     );
-//   };
-
-//   const handleAllDeleteClick = () => {
-//     localStorage.clear();
-//     setDataList([]);
-//   };
-
-//   return (
-//     <div>
-//       <h1>저장된 값 목록</h1>
-//       <button onClick={handleAllDeleteClick}>전체 삭제</button>
-//       <div>
-//         {dataList.map((data) => (
-//           <div key={data.key}>
-//             <div>
-//               {[data.percent, data.emotion_title].map((item, index) => (
-//                 <span key={index}>{item} </span>
-//               ))}
-//             </div>
-//             {data.emotion_text && <div>{data.emotion_text}</div>}
-//             <button onClick={() => handleDeleteClick(data.key)}>삭제</button>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// // export default SaveInfo;
-
-// import React, { useState, useEffect } from "react";
-// import "./SaveInfo.scss";
-
-// const SaveInfo = () => {
-//   const [dataList, setDataList] = useState([]);
-
-//   useEffect(() => {
-//     const keys = Object.keys(localStorage).filter(
-//       (key) => key.startsWith("bar_") || key.startsWith("about_")
-//     );
-//     const dataList = keys.map((key) => {
-//       const data = JSON.parse(localStorage.getItem(key));
-//       return { ...data, key };
-//     });
-//     setDataList(dataList);
-//   }, []);
-
-//   const handleDeleteClick = (key) => {
-//     localStorage.removeItem(key);
-//     setDataList((prevDataList) =>
-//       prevDataList.filter((data) => data.key !== key)
-//     );
-//   };
-
-//   const handleAllDeleteClick = () => {
-//     localStorage.clear();
-//     setDataList([]);
-//   };
-
-//   return (
-//     <div>
-//       <h1>저장된 값 목록</h1>
-//       <button onClick={handleAllDeleteClick}>전체 삭제</button>
-//       {dataList.map((data, index) => (
-//         <div key={data.key}>
-//           <div>
-//             {[`${data.percent}%`, data.emotion_title].map((item, index) => (
-//               <span key={index}>{item} </span>
-//             ))}
-//           </div>
-//           {data.emotion_text && <div>{data.emotion_text}</div>}
-//           {index === dataList.length - 1 && (
-//             <button onClick={() => handleDeleteClick(data.key)}>삭제</button>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default SaveInfo;
-
-// import React, { useState, useEffect } from "react";
-// import "./SaveInfo.scss";
-
-// const SaveInfo = () => {
-//   const [dataList, setDataList] = useState([]);
-
-//   useEffect(() => {
-//     const keys = Object.keys(localStorage).filter(
-//       (key) => key.startsWith("bar_") || key.startsWith("about_")
-//     );
-//     const dataList = keys.map((key) => {
-//       const data = JSON.parse(localStorage.getItem(key));
-//       return { ...data, key };
-//     });
-//     setDataList(dataList);
-//   }, []);
-
-//   const handleDeleteClick = (key) => {
-//     localStorage.removeItem(key);
-//     setDataList((prevDataList) =>
-//       prevDataList.filter((data) => data.key !== key)
-//     );
-//   };
-
-//   const handleAllDeleteClick = () => {
-//     localStorage.clear();
-//     setDataList([]);
-//   };
-
-//   return (
-//     <div>
-//       <h1>저장된 값 목록</h1>
-//       <button onClick={handleAllDeleteClick}>전체 삭제</button>
-//       {dataList.map((data, index) => (
-//         <div key={data.key}>
-//           <div>
-//             <span>{`${data.percent}% `}</span>
-//             <span>{data.emotion_title}</span>
-//           </div>
-//           {data.emotion_text && <div>{data.emotion_text}</div>}
-//           {index === dataList.length - 1 && (
-//             <button onClick={() => handleDeleteClick(data.key)}>삭제</button>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default SaveInfo;
-
 import React, { useState, useEffect } from "react";
 import "./SaveInfo.scss";
 
 const SaveInfo = () => {
-  const [dataList, setDataList] = useState([]);
+  const [percentList, setPercentList] = useState([]);
+  const [titleList, setTitleList] = useState([]);
+  const [textList, setTextList] = useState([]);
+  const [keyList, setKeyList] = useState([]);
 
   useEffect(() => {
     const keys = Object.keys(localStorage).filter(
       (key) => key.startsWith("bar_") || key.startsWith("about_")
     );
-    const dataList = keys.map((key) => {
+    const percentList = [];
+    const titleList = [];
+    const textList = [];
+    const keyList = [];
+    keys.forEach((key) => {
       const data = JSON.parse(localStorage.getItem(key));
-      return { ...data, key };
+      percentList.push(data.percent);
+      titleList.push(data.emotion_title);
+      textList.push(data.emotion_text || "");
+      keyList.push(key);
     });
-    setDataList(dataList);
+    setPercentList(percentList);
+    setTitleList(titleList);
+    setTextList(textList);
+    setKeyList(keyList);
   }, []);
 
   const handleDeleteClick = (key) => {
     localStorage.removeItem(key);
-    setDataList((prevDataList) =>
-      prevDataList.filter((data) => data.key !== key)
+    const idx = keyList.indexOf(key);
+    setPercentList((prevPercentList) =>
+      prevPercentList.filter((_, i) => i !== idx)
     );
-  };
-
-  const handleAllDeleteClick = () => {
-    localStorage.clear();
-    setDataList([]);
+    setTitleList((prevTitleList) => prevTitleList.filter((_, i) => i !== idx));
+    setTextList((prevTextList) => prevTextList.filter((_, i) => i !== idx));
+    setKeyList((prevKeyList) => prevKeyList.filter((_, i) => i !== idx));
   };
 
   return (
-    <div>
-      <h1>저장된 값 목록</h1>
-      <button onClick={handleAllDeleteClick}>전체 삭제</button>
-      {/* {dataList.map((data, index) => (
-        <div key={data.key}>
-          <div>
-            <span id="test"></span>
-            {data.emotion_text && <div>{data.emotion_text}</div>}
-          </div>
-          {index === dataList.length - 1 && (
-            <button onClick={() => handleDeleteClick(data.key)}>삭제</button>
-          )}
+    <div className="save-info">
+      <h2 className="save-info__title">Saved Emotions</h2>
+      {keyList.length ? (
+        <ul className="save-info__list">
+          {keyList.map((key, i) => (
+            <li className="save-info__item" key={key}>
+              <div className="save-info__header">
+                <div className="save-info__percent">
+                  {percentList[i] ? `${percentList[i]}%` : ""}
+                </div>
+                <div className="save-info__title">{titleList[i]}</div>
+                <button
+                  className="save-info__delete"
+                  onClick={() => handleDeleteClick(key)}
+                >
+                  Delete
+                </button>
+              </div>
+              {textList[i] && (
+                <div className="save-info__text">{textList[i]}</div>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="save-info__empty">
+          You haven't saved any emotions yet!
         </div>
-      ))} */}
-      {dataList.map((data, index) => (
-        <div key={data.key}>
-          <div>
-            <span>{data.percent}%</span>
-            <span>{data.text}</span>
-          </div>
-          {data.emotion_text && <div>{data.emotion_text}</div>}
-          {index === dataList.length - 1 && (
-            <button onClick={() => handleDeleteClick(data.key)}>삭제</button>
-          )}
-        </div>
-      ))}
+      )}
     </div>
   );
 };
